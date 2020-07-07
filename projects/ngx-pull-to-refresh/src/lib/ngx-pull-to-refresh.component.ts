fe76e31813643d0d29f4c226115f0a24420528db
YeongCheon <kyc1682@gmail.com>
2020-07-07 18:11:02 +0900

    [ENH] 스크롤, 터치 관련 이벤트 window대신 element에 적용

--- projects/ngx-pull-to-refresh/src/lib/ngx-pull-to-refresh.component.ts
+++ projects/ngx-pull-to-refresh/src/lib/ngx-pull-to-refresh.component.ts
@@ -2,144 +2,157 @@ import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output, HostLis
 import { Subject } from 'rxjs';
 
 @Component({
-  selector: 'ngx-pull-to-refresh',
-  templateUrl: './ngx-pull-to-refresh.component.html',
-  styleUrls: ['./ngx-pull-to-refresh.component.scss']
+    selector: 'ngx-pull-to-refresh',
+    templateUrl: './ngx-pull-to-refresh.component.html',
+    styleUrls: ['./ngx-pull-to-refresh.component.scss']
 })
 export class NgxPullToRefreshComponent implements OnInit {
-  @Input()
-  spinnerColor = '#F7C223';
-  @Input()
-  spinnerSize = '50px';
+    @Input()
+    spinnerColor = '#F7C223';
+    @Input()
+    spinnerSize = '50px';
 
-  private isRefresh = false;
-  private isScrollTop = false;
-  private isOnScrollBottom = false;
-  private lastScrollTop = 0;
-  @ViewChild('wrapper')
-  private wrapperElement: ElementRef;
-  @ViewChild('loadingContainer')
-  private loadingbar: ElementRef;
-  @ViewChild('circle')
-  private circleSvgElement: ElementRef;
+    private isRefresh = false;
+    private isScrollTop = false;
+    private isOnScrollBottom = false;
+    private lastScrollTop = 0;
+    @ViewChild('wrapper', { static: true })
+    private wrapperElement: ElementRef;
+    @ViewChild('loadingContainer')
+    private loadingbar: ElementRef;
+    @ViewChild('circle')
+    private circleSvgElement: ElementRef;
 
-  private touchStartScreenY = 0;
+    private touchStartScreenY = 0;
 
-  private readonly CIRCLE_OFFSET = 187;
-  private readonly DISTANCE_FOR_REFRESH = 40;
-  private readonly LOADINGBAR_DISPLAY_STYLE = 'flex';
+    private readonly CIRCLE_OFFSET = 187;
+    private readonly DISTANCE_FOR_REFRESH = 40;
+    private readonly LOADINGBAR_DISPLAY_STYLE = 'flex';
 
-  scrollPullPercent = 20;
-  isPlayingAnimation = false;
+    scrollPullPercent = 20;
+    isPlayingAnimation = false;
 
-  @Output() refresh: EventEmitter<any> = new EventEmitter<any>();
-  refreshCompleteSubject = new Subject();
-  @Output() loadMore: EventEmitter<any> = new EventEmitter<any>();
+    @Output() refresh: EventEmitter<any> = new EventEmitter<any>();
+    refreshCompleteSubject = new Subject();
+    @Output() loadMore: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() {
-  }
-
-  ngOnInit() {
-    this.refreshCompleteSubject.subscribe(() => {
-      this.isPlayingAnimation = false;
-      this.restoreWrapper();
-      this.restoreLoadingbar();
-    });
-  }
-
-  @HostListener('window:touchmove', ['$event'])
-  onTouchMove($event: any): void {
-    const moveYDistance: number = this.touchStartScreenY - $event.touches[0].screenY;
-    if (window.scrollY <= 0 && this.lastScrollTop <= 0) {
-      this.isScrollTop = true;
-    } else {
-      this.isScrollTop = false;
+    constructor() {
     }
 
-    if (this.isScrollTop && moveYDistance <= this.DISTANCE_FOR_REFRESH * -1) {
-      this.isRefresh = true;
-    } else {
-      this.isRefresh = false;
+    ngOnInit() {
+        this.refreshCompleteSubject.subscribe(() => {
+            this.isPlayingAnimation = false;
+            this.restoreWrapper();
+            this.restoreLoadingbar();
+        });
+
+        this.wrapperElement.nativeElement.addEventListener('touchstart', (evt: any) => {
+            this.onTouchStart(evt);
+        });
+        this.wrapperElement.nativeElement.addEventListener('touchmove', (evt: any) => {
+            this.onTouchMove(evt);
+        });
+        this.wrapperElement.nativeElement.addEventListener('scroll', (evt: any) => {
+            this.onScroll(evt);
+        });
+        this.wrapperElement.nativeElement.addEventListener('touchend', (evt: any) => {
+            this.onMouseup(evt);
+        });
     }
 
-    this.lastScrollTop = window.scrollY;
+    // @HostListener('window:touchmove', ['$event'])
+    onTouchMove($event: any): void {
+        const moveYDistance: number = this.touchStartScreenY - $event.touches[0].screenY;
+        if (window.scrollY <= 0 && this.lastScrollTop <= 0) {
+            this.isScrollTop = true;
+        } else {
+            this.isScrollTop = false;
+        }
 
-    this.moveWrapper(moveYDistance * -1);
+        if (this.isScrollTop && moveYDistance <= this.DISTANCE_FOR_REFRESH * -1) {
+            this.isRefresh = true;
+        } else {
+            this.isRefresh = false;
+        }
 
-    this.drawCircle(this.scrollPullPercent);
-  }
+        this.lastScrollTop = window.scrollY;
 
-  @HostListener('window:scroll', ['$event'])
-  onScroll($event: any): void {
-    this.isOnScrollBottom = window.scrollY >= 0 &&
-      (window.scrollY + window.innerHeight) >= document.scrollingElement.scrollHeight * 0.9;
+        this.moveWrapper(moveYDistance * -1);
 
-
-    if (this.isOnScrollBottom &&
-      this.loadMoreFunction &&
-      document.contains(this.wrapperElement.nativeElement)) {
-      this.loadMoreFunction();
-    }
-  }
-
-  @HostListener('window:touchstart', ['$event'])
-  onTouchStart($event: any): void {
-    this.isRefresh = false;
-    this.touchStartScreenY = $event.touches[0].screenY;
-  }
-
-  @HostListener('window:touchend', ['$event'])
-  onMouseup($event: any): void {
-    if (this.isRefresh && document.contains(this.wrapperElement.nativeElement)) {
-      this.refreshFunction();
-    } else {
-      this.restoreWrapper();
-      this.restoreLoadingbar();
-    }
-  }
-
-  moveWrapper(offsetY: number): void {
-    const loadingbar: HTMLElement = this.loadingbar.nativeElement;
-
-    let loadingbarY: number = offsetY;
-    if (offsetY >= this.DISTANCE_FOR_REFRESH) {
-      loadingbarY = this.DISTANCE_FOR_REFRESH;
+        this.drawCircle(this.scrollPullPercent);
     }
 
-    if (this.isScrollTop && offsetY >= 0) {
-      loadingbar.style.display = this.LOADINGBAR_DISPLAY_STYLE;
-      loadingbar.style.top = loadingbarY.toString() + 'px';
-      this.scrollPullPercent = (loadingbarY / this.DISTANCE_FOR_REFRESH) * 100;
+    // @HostListener('window:scroll', ['$event'])
+    onScroll($event: any): void {
+        this.isOnScrollBottom = window.scrollY >= 0 &&
+            (window.scrollY + window.innerHeight) >= document.scrollingElement.scrollHeight * 0.9;
+
+
+        if (this.isOnScrollBottom &&
+            this.loadMoreFunction &&
+            document.contains(this.wrapperElement.nativeElement)) {
+            this.loadMoreFunction();
+        }
     }
-  }
 
-  restoreWrapper(): void {
-    const wrapper: HTMLElement = this.wrapperElement.nativeElement;
-    const loadingbar: HTMLElement = this.loadingbar.nativeElement;
+    // @HostListener('window:touchstart', ['$event'])
+    onTouchStart($event: any): void {
+        this.isRefresh = false;
+        this.touchStartScreenY = $event.touches[0].screenY;
+    }
 
-    wrapper.style.marginTop = '0px';
-    loadingbar.style.display = 'none';
-  }
+    // @HostListener('window:touchend', ['$event'])
+    onMouseup($event: any): void {
+        if (this.isRefresh && document.contains(this.wrapperElement.nativeElement)) {
+            this.refreshFunction();
+        } else {
+            this.restoreWrapper();
+            this.restoreLoadingbar();
+        }
+    }
 
-  restoreLoadingbar(): void {
-    const loadingbar: HTMLElement = this.loadingbar.nativeElement;
-    loadingbar.style.display = 'none';
+    moveWrapper(offsetY: number): void {
+        const loadingbar: HTMLElement = this.loadingbar.nativeElement;
 
-    this.scrollPullPercent = 0;
-    this.drawCircle(this.scrollPullPercent);
-  }
+        let loadingbarY: number = offsetY;
+        if (offsetY >= this.DISTANCE_FOR_REFRESH) {
+            loadingbarY = this.DISTANCE_FOR_REFRESH;
+        }
 
-  refreshFunction(): void {
-    this.isPlayingAnimation = true;
-    this.refresh.emit(this.refreshCompleteSubject);
-  }
+        if (this.isScrollTop && offsetY >= 0) {
+            loadingbar.style.display = this.LOADINGBAR_DISPLAY_STYLE;
+            loadingbar.style.top = loadingbarY.toString() + 'px';
+            this.scrollPullPercent = (loadingbarY / this.DISTANCE_FOR_REFRESH) * 100;
+        }
+    }
 
-  loadMoreFunction(): void {
-    this.loadMore.emit(true);
-  }
+    restoreWrapper(): void {
+        const wrapper: HTMLElement = this.wrapperElement.nativeElement;
+        const loadingbar: HTMLElement = this.loadingbar.nativeElement;
 
-  private drawCircle(percentage: number) {
-    const offset = this.CIRCLE_OFFSET - (this.CIRCLE_OFFSET * (Math.abs(percentage) / 100));
-    this.circleSvgElement.nativeElement.style.strokeDashoffset = offset;
-  }
+        wrapper.style.marginTop = '0px';
+        loadingbar.style.display = 'none';
+    }
+
+    restoreLoadingbar(): void {
+        const loadingbar: HTMLElement = this.loadingbar.nativeElement;
+        loadingbar.style.display = 'none';
+
+        this.scrollPullPercent = 0;
+        this.drawCircle(this.scrollPullPercent);
+    }
+
+    refreshFunction(): void {
+        this.isPlayingAnimation = true;
+        this.refresh.emit(this.refreshCompleteSubject);
+    }
+
+    loadMoreFunction(): void {
+        this.loadMore.emit(true);
+    }
+
+    private drawCircle(percentage: number) {
+        const offset = this.CIRCLE_OFFSET - (this.CIRCLE_OFFSET * (Math.abs(percentage) / 100));
+        this.circleSvgElement.nativeElement.style.strokeDashoffset = offset;
+    }
 }
